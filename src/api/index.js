63740faa7117330cd047c1b3c507b29c94476410
stylessh <stylesshjs@gmail.com>
2020-11-27 14:38:50 -0400

    feat(search): All search logic is done. Except for the pagination.

--- src/api/index.js
+++ src/api/index.js
@@ -3,11 +3,15 @@ const proxy = "https://cors-anywhere.herokuapp.com/";
 // jobs api
 const api_url = "https://jobs.github.com/positions.json";
 
-export const getJobs = async (page, location) => {
-  const res = await fetch(
-    // api params
-    proxy + api_url + `?page=${page}&location=${location}`
-  );
+export const getJobs = async (page, location, title, fullTime) => {
+  // api params
+  const complete_api =
+    proxy +
+    api_url +
+    `?page=${page}&location=${location}&description=${title.toLowerCase()}&full_time=${fullTime}`;
+
+  const res = await fetch(complete_api);
+
   const data = await res.json();
 
   return data;
